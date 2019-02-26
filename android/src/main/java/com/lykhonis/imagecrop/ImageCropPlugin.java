23b1e828799dda4d2c2b4baf9ec4c66e56ccd259
Maxim Ermakov <ermakov@steelkiwi.com>
2019-02-26 15:25:25 +0200

    crash fix

--- android/src/main/java/com/lykhonis/imagecrop/ImageCropPlugin.java
+++ android/src/main/java/com/lykhonis/imagecrop/ImageCropPlugin.java
@@ -126,15 +126,14 @@ public final class ImageCropPlugin implements MethodCallHandler, PluginRegistry.
                 paint.setDither(true);
 
                 Rect srcRect = new Rect((int) (srcBitmap.getWidth() * area.left), (int) (srcBitmap.getHeight() * area.top),
-                        (int) (srcBitmap.getWidth() * area.right), (int) (srcBitmap.getHeight() * area.bottom));
-                Bitmap croppedBmp = Bitmap.createBitmap(srcBitmap, (int) (srcBitmap.getWidth() * area.left), (int) (srcBitmap.getHeight() * area.top), width, height);
+                                        (int) (srcBitmap.getWidth() * area.right), (int) (srcBitmap.getHeight() * area.bottom));
                 Rect dstRect = new Rect(0, 0, width, height);
+
                 canvas.drawBitmap(srcBitmap, srcRect, dstRect, paint);
-                Log.d(ImageCropPlugin.class.getSimpleName(),"srcW: "+srcBitmap.getWidth()+ "srcH: "+ srcBitmap.getHeight());
-                Log.d(ImageCropPlugin.class.getSimpleName(),"width: "+width+" Height: "+height );
+
                 try {
                     File dstFile = createTemporaryImageFile();
-                    compressBitmap(croppedBmp, dstFile);
+                    compressBitmap(dstBitmap, dstFile);
                     copyExif(srcFile, dstFile);
                     result.success(dstFile.getAbsolutePath());
                 } catch (IOException e) {
